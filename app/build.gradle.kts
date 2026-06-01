4a34941bf3eb95114e6f1632bf48c6272f05c53e
moychita <annisahdiani@gmail.com>
2026-06-01 18:32:45 +0800

    feat: initial project setup

---
+++ app/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.educonnect"
+    compileSdk {
+        version = release(36)
+    }
+
+    defaultConfig {
+        applicationId = "com.example.educonnect"
+        minSdk = 30
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    dependencies {
+        implementation("androidx.appcompat:appcompat:1.6.1")
+        implementation("com.google.android.material:material:1.11.0")
+        implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+        // Navigation Component
+        implementation("androidx.navigation:navigation-fragment:2.7.7")
+        implementation("androidx.navigation:navigation-ui:2.7.7")
+
+        // Retrofit
+        implementation("com.squareup.retrofit2:retrofit:2.9.0")
+        implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+        implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+        // RecyclerView
+        implementation("androidx.recyclerview:recyclerview:1.3.2")
+
+        // SwipeRefreshLayout
+        implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    }
+}
