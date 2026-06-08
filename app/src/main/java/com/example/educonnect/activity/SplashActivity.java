4a85c1c56fab2fa7b1282dc136d05765611aad08
moychita <annisahdiani@gmail.com>
2026-06-08 13:08:48 +0800

    fix: SplashActivity

--- app/src/main/java/com/example/educonnect/activity/SplashActivity.java
+++ app/src/main/java/com/example/educonnect/activity/SplashActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import com.example.educonnect.activity.MainActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
